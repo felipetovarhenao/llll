bb4ec514a35b6adedcfd75b68b4184421ad3c16c
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2025-11-06 22:48:53 -0500

    Add pretty printing

--- src/llll/__init__.py
+++ src/llll/__init__.py
@@ -175,21 +175,57 @@ class llll:
         items_repr = ' '.join(repr(item) for item in self._items)
         return f"[ {items_repr} ]"
 
+    def depth(self):
+        """Compute the maximum depth of nested lllls."""
+        if self.is_atomic():
+            return 0
+        if not self._items:
+            return 1
+        return 1 + max((item.depth() for item in self._items), default=0)
+
     def __str__(self):
         """User-friendly string representation."""
-        return self._to_str(top_level=True)
+        return self._to_str(top_level=True, indent=-1, min_depth=2)
 
-    def _to_str(self, top_level=False):
-        """Internal string conversion with top-level handling."""
+    def _to_str(self, top_level=False, indent=0, min_depth=2):
+        """Internal string conversion with top-level handling.
+
+        Args:
+            top_level: Whether this is the top-level call
+            indent: Current indentation level
+            min_depth: Minimum depth required to use indented formatting (default: 2)
+        """
         if self.is_atomic():
             return str(self._value)
 
-        items_str = ' '.join(item._to_str() for item in self._items)
+        # Check if we should use indented format
+        use_indented = self.depth() >= min_depth
 
-        # Top level doesn't need brackets
-        if top_level:
-            return items_str
-        return f"[ {items_str} ]"
+        if use_indented:
+            # Build indented representation
+            indent_str = '  ' * indent
+            next_indent = '  ' * (indent + 1)
+
+            items_str = []
+            for item in self._items:
+                item_repr = item._to_str(
+                    indent=indent + 1, min_depth=min_depth)
+                items_str.append(f'\n{next_indent}{item_repr}')
+
+            items_content = ''.join(items_str)
+
+            # Top level doesn't need brackets
+            if top_level:
+                return items_content.lstrip()
+            return f'[{items_content}\n{indent_str}]'
+        else:
+            # Use compact single-line format
+            items_str = ' '.join(item._to_str(min_depth=min_depth)
+                                 for item in self._items)
+
+            if top_level:
+                return items_str
+            return f'[ {items_str} ]'
 
     def to_python(self):
         """Convert to native Python list/value structure."""
@@ -290,5 +326,6 @@ class Parser:
 
 
 if __name__ == '__main__':
-    l = llll.from_file('./test.llll')
-    print(l[1]['duration'])
+    l = llll.from_file(
+        '/Users/felipetovarhenao/Documents/llll_files/nums.llll')
+    print(l)
